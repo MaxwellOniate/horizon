e39ae7480b5fb54862efa5a1f2656960bb39ad56
maxwelloniate <maxwelloniate@gmail.com>
2020-04-20 11:44:27 -0400

    Add theme supports

--- wp-content/themes/horizon/functions.php
+++ wp-content/themes/horizon/functions.php
@@ -1,9 +1,20 @@
 <?php
 
+add_theme_support('title-tag');
+add_theme_support('post-thumbnails');
+add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat']);
+add_theme_support('html5');
+add_theme_support('automatic-feed-links');
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('custom-logo');
+add_theme_support('customize-selective-refresh-widgets');
+add_theme_support('starter-content');
 
-function load_scripts_styles()
+
+function horizon_enqueue_styles()
 {
   wp_enqueue_style('style.css', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'load_scripts_styles');
+add_action('wp_enqueue_scripts', 'horizon_enqueue_styles');
