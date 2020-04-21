c932873a537154f38baa6aff5bdd376c98b9764d
maxwelloniate <maxwelloniate@gmail.com>
2020-04-21 13:19:52 -0400

    Page templates and parts, etc.

---
+++ wp-content/themes/horizon/template-parts/content.php
@@ -0,0 +1,12 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+  <header class="entry-header">
+    <h1><?php the_title(); ?></h1>
+    <small><?php esc_html_e('Author: ');
+            the_author(); ?></small>
+  </header>
+
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
+</article>
