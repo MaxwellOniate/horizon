ccb36dd0de91f9199757e9e0d36eb848c0e15d80
maxwelloniate <maxwelloniate@gmail.com>
2020-04-24 13:45:06 -0400

    Understrap starter theme

--- wp-content/themes/horizon/functions.php
+++ wp-content/themes/horizon/functions.php
@@ -1,20 +1,31 @@
 <?php
+/**
+ * UnderStrap functions and definitions
+ *
+ * @package understrap
+ */
 
-add_theme_support('title-tag');
-add_theme_support('post-thumbnails');
-add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat']);
-add_theme_support('html5');
-add_theme_support('automatic-feed-links');
-add_theme_support('custom-background');
-add_theme_support('custom-header');
-add_theme_support('custom-logo');
-add_theme_support('customize-selective-refresh-widgets');
-add_theme_support('starter-content');
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
+$understrap_includes = array(
+	'/theme-settings.php',                  // Initialize theme default settings.
+	'/setup.php',                           // Theme setup and custom theme supports.
+	'/widgets.php',                         // Register widget area.
+	'/enqueue.php',                         // Enqueue scripts and styles.
+	'/template-tags.php',                   // Custom template tags for this theme.
+	'/pagination.php',                      // Custom pagination for this theme.
+	'/hooks.php',                           // Custom hooks.
+	'/extras.php',                          // Custom functions that act independently of the theme templates.
+	'/customizer.php',                      // Customizer additions.
+	'/custom-comments.php',                 // Custom Comments file.
+	'/jetpack.php',                         // Load Jetpack compatibility file.
+	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567
+	'/woocommerce.php',                     // Load WooCommerce functions.
+	'/editor.php',                          // Load Editor functions.
+	'/deprecated.php',                      // Load deprecated functions.
+);
 
-function horizon_enqueue_styles()
-{
-  wp_enqueue_style('style.css', get_stylesheet_uri());
+foreach ( $understrap_includes as $file ) {
+	require_once get_template_directory() . '/inc' . $file;
 }
-
-add_action('wp_enqueue_scripts', 'horizon_enqueue_styles');
--- wp-content/themes/horizon/single.php
+++ wp-content/themes/horizon/single.php
@@ -1,16 +1,52 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @package understrap
+ */
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-  <?php get_template_part('template-parts/content'); ?>
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
 
-  <?php endwhile;
-else : ?>
+<div class="wrapper" id="single-wrapper">
 
-  <?php get_template_part('template-parts/content', 'none'); ?>
-  
-<?php endif; ?>
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
+		<div class="row">
 
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-<?php get_footer(); ?>
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
+
+					<?php understrap_post_nav(); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #single-wrapper -->
+
+<?php get_footer();
