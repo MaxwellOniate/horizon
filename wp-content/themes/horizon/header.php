4a5a8d8d6d054e785d353108b11a16ad9e869049
maxwelloniate <maxwelloniate@gmail.com>
2020-04-29 22:34:49 -0400

    Update

--- wp-content/themes/horizon/header.php
+++ wp-content/themes/horizon/header.php
@@ -32,10 +32,65 @@ $container = get_theme_mod('understrap_container_type');
 
 			<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'understrap'); ?></a>
 
-			<div id="main-nav-contact">
-				<div class="container">
-					<div class="row">
+			<nav id="main-nav-top" class="navbar navbar-expand navbar-light">
 
+				<?php if ('container' === $container) : ?>
+					<div class="container">
+					<?php endif; ?>
+
+					<ul class="navbar-nav">
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-facebook-f"></i>
+							</a>
+						</li>
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-twitter"></i>
+							</a>
+						</li>
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-google-plus"></i>
+							</a>
+						</li>
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-youtube"></i>
+							</a>
+						</li>
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-pinterest"></i>
+							</a>
+						</li>
+
+						<li class="nav-item">
+							<a href="#" class="nav-link">
+								<i class="fa fa-instagram"></i>
+							</a>
+						</li>
+
+					</ul>
+
+					<?php if ('container' === $container) : ?>
+					</div><!-- .container -->
+				<?php endif; ?>
+
+			</nav>
+
+			<div id="main-nav-middle">
+
+				<?php if ('container' === $container) : ?>
+					<div class="container">
+					<?php endif; ?>
+
+					<div class="row">
 						<div class="brand-container col-lg-4 align-self-center">
 							<!-- Your site title as branding in the menu -->
 							<?php if (!has_custom_logo()) { ?>
@@ -62,7 +117,7 @@ $container = get_theme_mod('understrap_container_type');
 								<div class="media">
 									<i class="fa fa-phone"></i>
 									<div class="media-body">
-										<span class="d-block">Call Us</span> 555-555-5555
+										<span><span class="d-block">Call Us</span> 555-555-5555</span>
 									</div>
 								</div>
 							</div>
@@ -71,7 +126,7 @@ $container = get_theme_mod('understrap_container_type');
 								<div class="media">
 									<i class="fa fa-envelope-square"></i>
 									<div class="media-body">
-										<span class="d-block">Email Us</span> info@canvas.com
+										<span><span class="d-block">Email Us</span> info@canvas.com</span>
 
 									</div>
 								</div>
@@ -81,15 +136,18 @@ $container = get_theme_mod('understrap_container_type');
 								<div class="media">
 									<i class="fa fa-clock-o"></i>
 									<div class="media-body">
-										<span class="d-block">We're Open</span> Mon - Sat, 10AM to 6PM
+										<span><span class="d-block">We're Open</span> Mon - Sat, 10AM to 6PM</span>
 									</div>
 								</div>
 							</div>
 
 						</div>
-
 					</div>
-				</div>
+
+					<?php if ('container' === $container) : ?>
+					</div><!-- .container -->
+				<?php endif; ?>
+
 			</div>
 
 			<nav id="main-nav" class="navbar navbar-expand-lg navbar-light bg-white" aria-labelledby="main-nav-label">
@@ -101,8 +159,6 @@ $container = get_theme_mod('understrap_container_type');
 				<?php if ('container' === $container) : ?>
 					<div class="container">
 					<?php endif; ?>
-
-
 
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
 						<span class="navbar-toggler-icon"></span>
