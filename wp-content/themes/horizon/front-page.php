e53d06b52aecb84e236908ced7c8aa308efd312b
maxwelloniate <maxwelloniate@gmail.com>
2020-05-02 22:31:32 -0400

    Showcase card

--- wp-content/themes/horizon/front-page.php
+++ wp-content/themes/horizon/front-page.php
@@ -2,9 +2,14 @@
 
 <div id="showcase" style="background-image:url('<?php echo get_theme_file_uri('/img/slider/2.jpg'); ?>')">
   <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <h1>Welcome to Horizon</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum repellendus dolorum maxime nisi soluta iusto dicta laudantium nostrum voluptas alias.</p>
+      </div>
+    </div>
 
   </div>
 </div>
-
 
 <?php get_footer(); ?>
