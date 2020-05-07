ed55f8c16e54fa0b7f2f858edc971ed86755fac0
maxwelloniate <maxwelloniate@gmail.com>
2020-05-06 20:36:19 -0400

    Call to action class name fixed

--- wp-content/themes/horizon/front-page.php
+++ wp-content/themes/horizon/front-page.php
@@ -14,9 +14,9 @@
 
 <div class="home-c2a bg-f5f5f5 py-5">
   <div class="container d-lg-flex align-items-center justify-content-between">
-    <div class="c2a-text">
-      <h3 class="c2a-text-heading">Start Planning Your New Dream Home With Us</h3>
-      <p class="c2a-text-body mb-4 mb-lg-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <div class="home-c2a-text">
+      <h3 class="home-c2a-text-heading">Start Planning Your New Dream Home With Us</h3>
+      <p class="home-c2a-text-body mb-4 mb-lg-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <a href="#" class="home-c2a-btn btn btn-outline-dark btn-lg">
       Start Now
