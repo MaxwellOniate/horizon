862ab419674e9ade8c0ad031708695fccc9239cf
maxwelloniate <maxwelloniate@gmail.com>
2020-05-08 12:30:59 -0400

    Reliability points section

--- wp-content/themes/horizon/front-page.php
+++ wp-content/themes/horizon/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section id="showcase" style="background-image:url('<?php echo get_theme_file_uri('/img/slider/2.jpg'); ?>')">
+<section class="home-showcase" style="background-image:url('<?php echo get_theme_file_uri('/img/slider/2.jpg'); ?>')">
   <div class="container">
     <div class="card">
       <div class="card-body">
@@ -478,5 +478,30 @@
   <hr>
 </div>
 
+<section class="home-points pt-5">
+  <div class="container">
+    <div class="row no-gutters">
+
+      <div class="d-none d-lg-block col-lg-7">
+        <img src="<?php echo get_theme_file_uri('/img/services/bottom-trust.jpg'); ?>" alt="Smiling construction guy.">
+      </div>
+
+      <div class="center col-lg-5 pb-5">
+        <div class="points-container">
+          <h3>You're in good hands.</h3>
+          <p>Five points that define our reliability.</p>
+          <ul class="points">
+            <li><i class="fa fa-check"></i> Market leader since 1915.</li>
+            <li><i class="fa fa-check"></i> Positive results within deadlines.</li>
+            <li><i class="fa fa-check"></i> 100% Reliability & Guarantee</li>
+            <li><i class="fa fa-check"></i> Dedicated professional team of 500+</li>
+            <li><i class="fa fa-check"></i> Unmatched after project support.</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
