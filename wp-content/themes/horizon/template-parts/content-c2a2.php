768f3d5a3824601082d26adb750a62c1bf16432f
maxwelloniate <maxwelloniate@gmail.com>
2020-05-09 12:34:02 -0400

    Moved into template-parts folder

--- wp-content/themes/horizon/template-parts/content-c2a2.php
+++ wp-content/themes/horizon/template-parts/content-c2a2.php
@@ -1,4 +1,4 @@
-<div class="c2a2 py-5">
+<div class="c2a2">
   <div class="container">
     <span>Would you like to build your dream home with us? <a href="<?php echo esc_url(site_url('/contact')); ?>"><strong>Enquire here</strong></a></span>
   </div>
