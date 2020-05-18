bbedace303a8856e91e404da1607645792403cc1
maxwelloniate <maxwelloniate@gmail.com>
2020-05-17 22:14:46 -0400

    Archive pageBanner, post thumbnail styles, etc

--- wp-content/themes/horizon/loop-templates/content.php
+++ wp-content/themes/horizon/loop-templates/content.php
@@ -10,55 +10,131 @@
 defined('ABSPATH') || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('content-posts'); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
+	<?php if (has_post_thumbnail()) {
+	?>
 
-		<?php
-		the_title(
-			sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
-			'</a></h2>'
-		);
-		?>
+		<div class="media">
 
-		<?php if ('post' == get_post_type()) : ?>
+			<div class="entry-img">
+				<?php echo get_the_post_thumbnail($post->ID); ?>
+			</div>
 
-			<div class="entry-meta">
-				<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
+			<div class="media-body">
 
-				<span><a href="<?php get_the_author_posts_link(); ?>"><i class="fa fa-user"></i> <?php the_author(); ?></a></span>
+				<header class="entry-header">
 
-				<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
+					<?php
+					the_title(
+						sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+						'</a></h2>'
+					);
+					?>
 
-				<span>
-					<a href="<?php echo get_comments_link(); ?>">
-						<i class="fa fa-comment"></i>
-						<?php echo get_comments_number(); ?>
-					</a>
-				</span>
+					<?php if ('post' == get_post_type()) : ?>
 
-			</div><!-- .entry-meta -->
+						<div class="entry-meta">
+							<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
 
-		<?php endif; ?>
+							<span>
+								<i class="fa fa-user"></i>
+								<?php the_author_posts_link(); ?>
+							</span>
 
-	</header><!-- .entry-header -->
+							<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
 
-	<?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
+							<span>
+								<a href="<?php echo get_comments_link(); ?>">
+									<i class="fa fa-comment"></i>
+									<?php echo get_comments_number(); ?>
+								</a>
+							</span>
 
-	<div class="entry-content">
+						</div><!-- .entry-meta -->
 
-		<?php the_excerpt(); ?>
+					<?php endif; ?>
 
-		<?php
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
-				'after'  => '</div>',
-			)
-		);
-		?>
+				</header><!-- .entry-header -->
 
-	</div><!-- .entry-content -->
+
+
+				<div class="entry-content">
+
+					<?php the_excerpt(); ?>
+
+					<?php
+					wp_link_pages(
+						array(
+							'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+							'after'  => '</div>',
+						)
+					);
+					?>
+
+				</div><!-- .entry-content -->
+
+			</div>
+		</div>
+
+	<?php
+	} else {
+	?>
+		<header class="entry-header">
+
+			<?php
+			the_title(
+				sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
+				'</a></h2>'
+			);
+			?>
+
+			<?php if ('post' == get_post_type()) : ?>
+
+				<div class="entry-meta">
+					<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
+
+					<span>
+						<i class="fa fa-user"></i>
+						<?php the_author_posts_link(); ?>
+					</span>
+
+					<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
+
+					<span>
+						<a href="<?php echo get_comments_link(); ?>">
+							<i class="fa fa-comment"></i>
+							<?php echo get_comments_number(); ?>
+						</a>
+					</span>
+
+				</div><!-- .entry-meta -->
+
+			<?php endif; ?>
+
+		</header><!-- .entry-header -->
+
+
+		<div class="entry-content">
+
+			<?php the_excerpt(); ?>
+
+			<?php
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+					'after'  => '</div>',
+				)
+			);
+			?>
+
+		</div><!-- .entry-content -->
+	<?php
+	}
+	?>
+
+
+
 
 
 </article><!-- #post-## -->
