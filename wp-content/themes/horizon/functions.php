a6ff616a9bf7afe704175e5e01bc137c0a202421
maxwelloniate <maxwelloniate@gmail.com>
2020-05-25 12:51:09 -0400

    displayPostMeta

--- wp-content/themes/horizon/functions.php
+++ wp-content/themes/horizon/functions.php
@@ -69,5 +69,31 @@ function pageBanner($args = NULL)
 			</span>
 		</div>
 	</header>
-<?php
+	<?php
+}
+
+function displayPostMeta()
+{
+	if ('post' == get_post_type()) : ?>
+
+		<div class="entry-meta">
+			<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
+
+			<span>
+				<i class="fa fa-user"></i>
+				<?php the_author_posts_link(); ?>
+			</span>
+
+			<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
+
+			<span>
+				<a href="<?php echo get_comments_link(); ?>">
+					<i class="fa fa-comment"></i>
+					<?php echo get_comments_number(); ?>
+				</a>
+			</span>
+
+		</div><!-- .entry-meta -->
+
+<?php endif;
 }
--- wp-content/themes/horizon/loop-templates/content.php
+++ wp-content/themes/horizon/loop-templates/content.php
@@ -10,14 +10,14 @@
 defined('ABSPATH') || exit;
 ?>
 
-<article <?php post_class('content-posts'); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('posts'); ?> id="post-<?php the_ID(); ?>">
 
 	<?php if (has_post_thumbnail()) {
 	?>
 
 		<div class="media">
 
-			<div class="entry-img">
+			<div class="post-side-img">
 				<?php echo get_the_post_thumbnail($post->ID); ?>
 			</div>
 
@@ -30,30 +30,9 @@ defined('ABSPATH') || exit;
 						sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
 						'</a></h2>'
 					);
+
+					displayPostMeta();
 					?>
-
-					<?php if ('post' == get_post_type()) : ?>
-
-						<div class="entry-meta">
-							<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
-
-							<span>
-								<i class="fa fa-user"></i>
-								<?php the_author_posts_link(); ?>
-							</span>
-
-							<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
-
-							<span>
-								<a href="<?php echo get_comments_link(); ?>">
-									<i class="fa fa-comment"></i>
-									<?php echo get_comments_number(); ?>
-								</a>
-							</span>
-
-						</div><!-- .entry-meta -->
-
-					<?php endif; ?>
 
 				</header><!-- .entry-header -->
 
@@ -87,30 +66,8 @@ defined('ABSPATH') || exit;
 				sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),
 				'</a></h2>'
 			);
+			displayPostMeta();
 			?>
-
-			<?php if ('post' == get_post_type()) : ?>
-
-				<div class="entry-meta">
-					<span><i class="fa fa-calendar"></i> <?php the_date(); ?></span>
-
-					<span>
-						<i class="fa fa-user"></i>
-						<?php the_author_posts_link(); ?>
-					</span>
-
-					<span><i class="fa fa-folder-open"></i> <?php the_category(", "); ?></span>
-
-					<span>
-						<a href="<?php echo get_comments_link(); ?>">
-							<i class="fa fa-comment"></i>
-							<?php echo get_comments_number(); ?>
-						</a>
-					</span>
-
-				</div><!-- .entry-meta -->
-
-			<?php endif; ?>
 
 		</header><!-- .entry-header -->
 
