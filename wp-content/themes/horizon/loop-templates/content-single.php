983d65a54c012d25a904dd9aa30a6e3c793fb4a4
maxwelloniate <maxwelloniate@gmail.com>
2020-05-25 12:58:05 -0400

    Single post

--- wp-content/themes/horizon/loop-templates/content-single.php
+++ wp-content/themes/horizon/loop-templates/content-single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single post partial template
  *
@@ -6,24 +7,28 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
+	<div class="single-post-img">
+		<?php echo get_the_post_thumbnail($post->ID); ?>
+	</div>
+
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
 
-		<div class="entry-meta">
+		the_title('<h1 class="entry-title">', '</h1>');
 
-			<?php understrap_posted_on(); ?>
+		displayPostMeta();
 
-		</div><!-- .entry-meta -->
+		?>
+
+
 
 	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content">
 
@@ -32,18 +37,12 @@ defined( 'ABSPATH' ) || exit;
 		<?php
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
 				'after'  => '</div>',
 			)
 		);
 		?>
 
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
